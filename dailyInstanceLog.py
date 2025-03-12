80685fb64635064cc1f0fd01c7265974a1f691a2
TriedThrice <justingreen24@gmail.com>
2025-03-12 15:30:34 +1100

    Added compatibility for OAuth secure instances, I don't have a secure instance to test this on so I'm not sure if it works.

--- dailyInstanceLog.py
+++ dailyInstanceLog.py
@@ -2,23 +2,31 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Adjust these details for each instance
+
 INSTANCE_NAME = "dev290564"
 USERNAME = "admin"
 PASSWORD = "eg-/Tyt7ZL5I"
 
+# Details for standard instance
 API_URL = f"https://{INSTANCE_NAME}.service-now.com/api/now/table/sys_user"
 FILTER = "?sysparm_query=roles%3Dadmin&sysparm_view="
 HEADER = {"Accept": "application/json"}
+
+# Details for OAuth verification
+OAUTH_URL = f"https://{INSTANCE_NAME}.service-now.com/oauth_token.do"
+OAUTH_DATA = {
+    "grant_type": "password",
+    "client_id": "client_id",
+    "client_secret": "client_secret",
+    "username": USERNAME,
+    "password": PASSWORD   
+}
 
 # Create list to store user info for later comparison
 checkUserList = []
 
 # Make API request
 response = requests.get(API_URL + FILTER, auth=(USERNAME, PASSWORD), headers=HEADER)
-#response = requests.get(API_URL, auth=(USERNAME, PASSWORD), headers=HEADER)
-john = ["John Doe", "JohnDoe@gmail.com", "John Doe", "1234567890"]
-checkUserList.append(john)
 
 # Get XML file from the current instance 
 # Response from instance, code 200 is success
@@ -35,6 +43,28 @@ if response.status_code == 200:
         checkUserList.append(addUser)
 else:
     print("Error:", response.status_code, response.text)
+    # On fail, print error message then try to get OAuth token
+    oauth_response = requests.post(OAUTH_URL, data=OAUTH_DATA)
+    # If OAuth token is successful, get the user info
+    if oauth_response.status_code == 200:
+        oauth_token = oauth_response.json().get("access_token")
+        oauth_header = {"Accept": "application/json", "Authorization": f"Bearer {oauth_token}"}
+        response = requests.get(API_URL + FILTER, headers=oauth_header)
+        if response.status_code == 200:
+            users = response.json().get("result", [])
+            for user in users:
+                nameID = user.get("user_name")
+                email = user.get("email")
+                realname = user.get("name")
+                sysID = user.get("sys_id")
+        
+                addUser = [nameID, email, realname, sysID]
+        
+                checkUserList.append(addUser)
+        else:
+            print("Error:", response.status_code, response.text)
+    else:
+        print("Error:", oauth_response.status_code, oauth_response.text)
     
 defaultUserList = []
 
@@ -71,4 +101,4 @@ else:
     # send message to admin on service now instance using REST API, email, or other method
     # for now, just print the message
     
-    
+
